c297fdd150e5daca70fb553753b4ceb9fb308659
Robin Ou <82324767@qq.com>
2018-03-22 22:26:38 +0800

    :)

---
+++ com.gesila.test.guard.navigator.ui/src/main/java/com/gesila/test/guard/navigator/ui/views/GesilaTestGuardCommonNavigator.java
@@ -0,0 +1,26 @@
+package com.gesila.test.guard.navigator.ui.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.navigator.CommonNavigator;
+import org.eclipse.ui.part.ViewPart;
+
+/**
+ * 
+ * @author robin
+ *
+ */
+public class GesilaTestGuardCommonNavigator extends CommonNavigator {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		super.createPartControl(parent);
+
+	}
+
+	@Override
+	public void setFocus() {
+		super.setFocus();
+
+	}
+
+}
