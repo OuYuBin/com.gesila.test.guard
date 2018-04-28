aa9d1ad91e5787577fa33175d5f8ab41b0913790
robin <82324767@qq.com>
2018-04-28 09:17:29 +0800

    ==

--- com.gesila.test.guard.layout/src/main/java/com/gesila/test/guard/layout/GesilaBoderLayout.java
+++ com.gesila.test.guard.layout/src/main/java/com/gesila/test/guard/layout/GesilaBoderLayout.java
@@ -2,6 +2,7 @@ package com.gesila.test.guard.layout;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Layout;
@@ -12,12 +13,14 @@ import org.eclipse.swt.widgets.Layout;
  *
  */
 public class GesilaBoderLayout extends Layout {
-	
-	
-	private Control[] controls=new Control[5];
-	
+
+	private Control[] controls = new Control[5];
 
 	private Point[] sizes;
+
+	private int width;
+
+	private int height;
 
 	@Override
 	protected Point computeSize(Composite composite, int wHint, int hHint, boolean flushCache) {
@@ -27,34 +30,54 @@ public class GesilaBoderLayout extends Layout {
 	@Override
 	protected void layout(Composite composite, boolean flushCache) {
 		Control[] children = composite.getChildren();
+		if (sizes == null) {
+			initialize(children);
+		}
+
+		Rectangle rectangle = composite.getClientArea();
+		if (controls[GesilaBorderData.NORTH] != null) {
+			controls[GesilaBorderData.NORTH].setBounds(rectangle.x, rectangle.y, rectangle.width,
+					sizes[GesilaBorderData.NORTH].y);
+		}
+		if (controls[GesilaBorderData.WEST] != null) {
+			controls[GesilaBorderData.WEST].setBounds(rectangle.x, rectangle.y + sizes[GesilaBorderData.NORTH].y,
+					sizes[GesilaBorderData.WEST].x,
+					rectangle.height - sizes[GesilaBorderData.NORTH].y - sizes[GesilaBorderData.SOURTH].y);
+		}
+
+	}
+
+	private void initialize(Control[] children) {
 		for (Control child : children) {
 			Object layoutData = child.getLayoutData();
 			if (layoutData == null || !(layoutData instanceof GesilaBorderData)) {
 				continue;
 			}
-			GesilaBorderData gesilaBorderData=(GesilaBorderData)layoutData;
-			if(gesilaBorderData.getRegion()<0||gesilaBorderData.getRegion()>4) {
+			GesilaBorderData gesilaBorderData = (GesilaBorderData) layoutData;
+			if (gesilaBorderData.getRegion() < 0 || gesilaBorderData.getRegion() > 4) {
 				continue;
 			}
-			controls[gesilaBorderData.getRegion()]=child;
+			controls[gesilaBorderData.getRegion()] = child;
 		}
-		
-		sizes=new Point[5];
-		int i=0;
-		for(Control control:controls) {
-			if(control==null)
-				sizes[i]=new Point(0, 0);
-			else
-				sizes[i]=control.computeSize(SWT.DEFAULT, SWT.DEFAULT, true);
-		}
-		
-		int width=0;
-		
-		int height=0;
-		
-		width=Math.max(width, sizes[GesilaBorderData.NORTH].x);
 
-		height=Math.max(height, )
+		sizes = new Point[5];
+		int i = 0;
+		for (Control control : controls) {
+			if (control == null)
+				sizes[i] = new Point(0, 0);
+			else
+				sizes[i] = control.computeSize(SWT.DEFAULT, SWT.DEFAULT, true);
+		}
+
+		width = Math.max(width, sizes[GesilaBorderData.NORTH].x);
+
+		width = Math.max(width,
+				sizes[GesilaBorderData.WEST].x + sizes[GesilaBorderData.CENTER].x + sizes[GesilaBorderData.EAST].x);
+
+		width = Math.max(width, sizes[GesilaBorderData.SOURTH].x);
+
+		height = Math.max(Math.max(sizes[GesilaBorderData.WEST].y, sizes[GesilaBorderData.EAST].y),
+				sizes[GesilaBorderData.NORTH].y + sizes[GesilaBorderData.CENTER].y + sizes[GesilaBorderData.SOURTH].y);
 	}
 
 }
